8e733f337953714af6c9e8a00a11fdac0f19ba9f
robbytrujillo <robbyikusuma88@gmail.com>
2025-10-05 10:44:15 +0700

    introduction luxhotel app | Update and Delete Hotel Room Data

--- resources/views/admin/hotel_rooms/show.blade.php
+++ resources/views/admin/hotel_rooms/show.blade.php
@@ -1,47 +1,49 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex flex-row justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <div class="flex flex-row items-center justify-between">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 {{ __('Details Hotel') }}
             </h2>
         </div>
     </x-slot>
     
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="flex flex-col p-10 overflow-hidden bg-white shadow-sm sm:rounded-lg gap-y-5">
 
 
-                <div class="item-card flex flex-row justify-between items-center">
+                <div class="flex flex-row items-center justify-between item-card">
                     <div class="flex flex-row items-center gap-x-3">
                         <img src="{{ Storage::url($hotelRoom->thumbnail) }}" alt="" class="rounded-2xl object-cover w-[120px] h-[90px]">
                         <div class="flex flex-col">
-                            <h3 class="text-indigo-950 text-xl font-bold">
+                            <h3 class="text-xl font-bold text-indigo-950">
                                 {{$hotelRoom->name}}
                             </h3>
-                        <p class="text-slate-500 text-sm">
+                        <p class="text-sm text-slate-500">
                             {{ $hotel->city->name }}, {{ $hotel->country->name }}
                         </p>
                         </div>
                     </div> 
-                    <div  class="hidden md:flex flex-col">
-                        <p class="text-slate-500 text-sm">Price</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">
+                    <div  class="flex-col hidden md:flex">
+                        <p class="text-sm text-slate-500">Price</p>
+                        <h3 class="text-xl font-bold text-indigo-950">
                             Rp {{ number_format($hotel->getLowestRoomPrice(), 0, ',', '.') }}/night
                         </h3>
                     </div>
-                    <div  class="hidden md:flex flex-col">
-                        <p class="text-slate-500 text-sm">Star</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">
+                    <div  class="flex-col hidden md:flex">
+                        <p class="text-sm text-slate-500">Star</p>
+                        <h3 class="text-xl font-bold text-indigo-950">
                             2 star
                         </h3>
                     </div>
-                    <div class="hidden md:flex flex-row items-center gap-x-3">
-                        <a href="{{ route('admin.hotel_rooms.edit', [$hotel->slug, $room]) }}" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                    <div class="flex-row items-center hidden md:flex gap-x-3">
+                        <a href="{{ route('admin.hotel_rooms.edit', [$hotel->slug, $room]) }}" class="px-6 py-4 font-bold text-white bg-indigo-700 rounded-full">
                             Edit
                         </a>
                         <form action="{{ route('admin.hotel_rooms.destroy', [$hotel->slug, $room]) }}" method="POST"> 
-                            <button type="submit" class="font-bold py-4 px-6 bg-red-700 text-white rounded-full">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="px-6 py-4 font-bold text-white bg-red-700 rounded-full">
                                 Delete
                             </button>
                         </form>
@@ -49,7 +51,7 @@
                 </div>
 
                 <hr class="my-5">
-                <h3 class="text-indigo-950 text-xl font-bold">Gallery Photos</h3>
+                <h3 class="text-xl font-bold text-indigo-950">Gallery Photos</h3>
 
                 <div class="flex flex-row gap-x-5"> 
                         <img src=" " alt="" class="rounded-2xl object-cover w-[120px] h-[90px]">
@@ -57,16 +59,16 @@
                 </div>
 
                 <div>
-                    <h3 class="text-indigo-950 text-xl font-bold">Address</h3>
+                    <h3 class="text-xl font-bold text-indigo-950">Address</h3>
                 <p>
                     sadsasa asdasd 12
                 </p>
                 </div>
 
                 <hr class="my-5">
-                <div class="flex flex-row justify-between items-center">
-                    <h3 class="text-indigo-950 text-xl font-bold">Rooms Available</h3>
-                    <a href=" " class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                <div class="flex flex-row items-center justify-between">
+                    <h3 class="text-xl font-bold text-indigo-950">Rooms Available</h3>
+                    <a href=" " class="px-6 py-4 font-bold text-white bg-indigo-700 rounded-full">
                         Add New Room
                     </a>
                 </div>
